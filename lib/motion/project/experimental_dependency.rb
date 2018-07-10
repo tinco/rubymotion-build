3eb43f205a85c6454be53f438063eb6a97b0ea97
Tinco Andringa <mail@tinco.nl>
2018-07-10 02:14:27 +0200

    clean up output and extra code

--- lib/motion/project/experimental_dependency.rb
+++ lib/motion/project/experimental_dependency.rb
@@ -67,39 +67,7 @@ module Motion; module Project
         consts_referred[f[:name]] = f[:references]
       end
 
-      puts "---- Experimental: "
-      puts "consts_defined: #{consts_defined.inspect}"
-      puts "----"
-      puts "consts_referred: #{consts_referred.inspect}"
-      puts "----"
-
       dependency = establish_dependencies(build_context)
-
-      puts "external_references: #{@external_references.inspect}"
-      puts "----"
-
-
-      puts "Result: "
-      puts dependency.inspect
-      exit 1
-
-      # Expected result:
-      #       ----
-      # consts_defined: {"AppDelegate"=>"./app/app_delegate.rb", "All"=>"./app/beer.rb", "Beer"=>"./app/beer.rb", "Beer::All"=>"./app/beer.rb", "BeerDetailsController"=>"./app/beer_details_controller.rb", "CELLID"=>"./app/beer_list_controller.rb", "BeerListController"=>"./app/beer_list_controller.rb", "BeerListController::CELLID"=>"./app/beer_list_controller.rb", "ViewIdentifier"=>"./app/beer_map_controller.rb", "BeerMapController"=>"./app/beer_map_controller.rb", "BeerMapController::ViewIdentifier"=>"./app/beer_map_controller.rb"}
-      # ----
-      # consts_referred: {"UIWindow"=>["./app/app_delegate.rb"], "UIScreen"=>["./app/app_delegate.rb"], "UITabBarController"=>["./app/app_delegate.rb"], "BeerMapController"=>["./app/app_delegate.rb"], "BeerListController"=>["./app/app_delegate.rb"], "UINavigationController"=>["./app/app_delegate.rb"], "BeerDetailsController"=>["./app/app_delegate.rb"], "CLLocationCoordinate2D"=>["./app/beer.rb", "./app/beer_map_controller.rb"], "NSURL"=>["./app/beer.rb"], "UIViewController"=>["./app/beer_details_controller.rb", "./app/beer_map_controller.rb"], "UIWebView"=>["./app/beer_details_controller.rb"], "NSURLRequest"=>["./app/beer_details_controller.rb"], "UITableViewController"=>["./app/beer_list_controller.rb"], "UITabBarItem"=>["./app/beer_list_controller.rb", "./app/beer_map_controller.rb"], "UIImage"=>["./app/beer_list_controller.rb", "./app/beer_map_controller.rb"], "Beer"=>["./app/beer_list_controller.rb", "./app/beer_list_controller.rb", "./app/beer_list_controller.rb", "./app/beer_map_controller.rb"], "All"=>["./app/beer_list_controller.rb", "./app/beer_list_controller.rb", "./app/beer_list_controller.rb", "./app/beer_map_controller.rb"], "Beer::All"=>["./app/beer_list_controller.rb", "./app/beer_list_controller.rb", "./app/beer_list_controller.rb", "./app/beer_list_controller.rb", "./app/beer_list_controller.rb", "./app/beer_list_controller.rb", "./app/beer_map_controller.rb"], "CELLID"=>["./app/beer_list_controller.rb", "./app/beer_list_controller.rb"], "UITableViewCell"=>["./app/beer_list_controller.rb"], "UITableViewCellStyleDefault"=>["./app/beer_list_controller.rb"], "UITableViewCellAccessoryDetailDisclosureButton"=>["./app/beer_list_controller.rb"], "UITableViewCellSelectionStyleNone"=>["./app/beer_list_controller.rb"], "UIApplication"=>["./app/beer_list_controller.rb", "./app/beer_map_controller.rb"], "MKMapView"=>["./app/beer_map_controller.rb"], "ViewIdentifier"=>["./app/beer_map_controller.rb", "./app/beer_map_controller.rb"], "MKPinAnnotationView"=>["./app/beer_map_controller.rb"], "UIButton"=>["./app/beer_map_controller.rb"], "UIButtonTypeDetailDisclosure"=>["./app/beer_map_controller.rb"], "UIControlEventTouchUpInside"=>["./app/beer_map_controller.rb"]}
-      # ----
-      # Result:
-      # {"./app/beer_list_controller.rb"=>["./app/beer.rb"], "./app/beer_map_controller.rb"=>["./app/beer.rb"], "./app/app_delegate.rb"=>["./app/beer_details_controller.rb", "./app/beer_list_controller.rb", "./app/beer_map_controller.rb"]}
-
-      #
-      # ---- Old Experimental results:
-      # consts_defined: {"AppDelegate"=>["./app/app_delegate.rb"], "Beer"=>["./app/beer.rb"], "BeerDetailsController"=>["./app/beer_details_controller.rb"], "BeerListController"=>["./app/beer_list_controller.rb"], "BeerMapController"=>["./app/beer_map_controller.rb"]}
-      # ----
-      # consts_referred: {"UIViewController"=>["./app/beer_details_controller.rb", "./app/beer_map_controller.rb"], "UITableViewController"=>["./app/beer_list_controller.rb"]}
-      # ----
-      # Result:
-      # {}
 
       dependency
     end
@@ -141,8 +109,10 @@ module Motion; module Project
       build_context.each do |f|
         references = f[:references]
         dependants[f[:name]] =
-          references.map {|r| get_dependency(f[:name], definitions, r)}.compact
+          references.map {|r| get_dependency(f[:name], definitions, r)}.compact.uniq.select{|r| r != f[:name] }
       end
+
+      dependants.keep_if {|_,v| !v.empty? }
 
       dependants
     end
@@ -232,7 +202,6 @@ module Motion; module Project
       end
 
       # inside those matches we want to recurse to find more definitions and references
-
 
       {
         definitions: definitions,
@@ -411,117 +380,6 @@ module Motion; module Project
         definitions: {},
         references: {}
       }
-    end
-
-    def list_files(path)
-      Dir.glob("#{path}/**/*")
-    end
-
-    class Constant
-      attr_accessor :defined
-      attr_accessor :referred
-
-      def initialize(source)
-        @defined = []
-        @referred = []
-
-        evaluate_sexp(Ripper.sexp_raw(source))
-      end
-
-      def evaluate_sexp(sexp, parents = [])
-        # We do not want to modify the original array
-        parents = parents.dup
-
-        return unless sexp.is_a?(Array)
-
-        case sexp[0]
-        # Ignore code inside method definitions
-        # def foo; end
-        # def self.foo; end
-        when :def, :defs
-          return
-        # class A; end
-        # module A; end
-        when :class, :module
-          klass = get_full_const_path(sexp[1])
-          register_referred_constants(parents, klass.dup.tap { |a| a.pop })
-          parents.concat(klass)
-          @defined << parents.join('::')
-          if sexp[0] == :class
-            superclass = get_full_const_path(sexp[2]).join('::') if sexp[2]
-            @referred << superclass if superclass
-            evaluate_sexp(sexp[3], parents)
-          else
-            evaluate_sexp(sexp[2], parents)
-          end
-        # A, ::A, A::B, ::A::B
-        when :const_path_ref, :var_ref, :top_const_ref
-          const_path = get_full_const_path(sexp)
-          parents = [] if sexp.flatten.include?(:top_const_ref)
-          register_referred_constants(parents, const_path)
-        # A = 1, ::A = 1, A::B = 1, ::A::B = 1
-        when :const_path_field, :var_field, :top_const_field
-          const = get_full_const_path(sexp)
-          parents = [] if sexp.flatten.include?(:top_const_ref) || sexp.flatten.include?(:top_const_field)
-          path = (parents + const).join('::')
-          @referred.delete(path)
-          register_defined_constants(parents, const)
-          const.pop
-          register_referred_constants(parents, const)
-        else
-          # if it is ant other type, continue evaluating
-          sexp.count.times do |i|
-            evaluate_sexp(sexp[i], parents) if sexp[i].is_a?(Array)
-          end
-        end
-      end
-
-      # Get a full constant path (E.g. A::B::C) from a sexp chain
-      def get_full_const_path(sexp, const = [])
-        case sexp[0]
-        when :var_ref, :top_const_ref, :const_ref
-          if sexp[1][0] == :@const
-            const << sexp[1][1]
-          end
-        when :const_path_ref, :const_path_field
-          const << sexp[2][1]
-          get_full_const_path(sexp[1], const)
-        end
-        const.reverse
-      end
-
-      def register_defined_constants(parents, klasses)
-        construct_nest_constants!(@defined, parents, klasses)
-      end
-
-      def register_referred_constants(parents, klasses)
-        # Do not register a reference if the class itself is contained in the
-        # nesting chain. E.g.:
-        #
-        # class A; class B; class C
-        #   B::C
-        # end; end; end
-        #
-        # A::B::C::B::C == A::B::C
-        #
-        klasses = klasses.dup
-        while parents.include?(klasses.first)
-          klasses.shift
-        end
-        return if klasses.empty?
-
-        construct_nest_constants!(@referred, parents, klasses)
-      end
-
-      def construct_nest_constants!(consts, parents, klasses)
-        chain = klasses.dup
-        while !chain.empty?
-          path = (parents + chain).join('::')
-          consts << path if !consts.include?(path)
-          @referred.delete(path) if @defined.include?(path)
-          chain.pop
-        end
-      end
     end
   end
 end; end
