10e646bb76cebaf4f91ba1e11d6fa289f1055420
Tinco Andringa <mail@tinco.nl>
2018-07-10 13:38:22 +0200

    clean up matches

--- lib/motion/project/experimental_dependency.rb
+++ lib/motion/project/experimental_dependency.rb
@@ -56,20 +56,253 @@ module Motion; module Project
     end
 
     def run
-      parsed_files = @file_paths.map {|p| parse(p) }
-      build_context = parsed_files.map {|p| determine_definitions_and_references(p) }
+      analyzers = @file_paths.map {|f| ConstantsAnalyzer.new(f) }
+      analyzers.each(&:run)
+      establish_dependencies(analyzers)
+    end
 
-      # first we have to establish for each file which files it depends on
-
-      consts_defined  = build_context.flat_map {|f| f[:definitions] }
-      consts_referred = {}
-      build_context.each do |f|
-        consts_referred[f[:name]] = f[:references]
+    # ConstantsAnalyzer is used to find constant definitions and references in a Ruby file
+    class ConstantsAnalyzer
+      attr_reader :file_name, :parsed, :definitions, :references
+      def initialize(file_name)
+        @file_name = file_name
+        @parsed = []
+        @definitions = {}
+        @references = {}
       end
 
-      dependency = establish_dependencies(build_context)
+      def run
+        parse
+        determine_definitions_and_references
+      end
 
-      dependency
+      def parse
+        @parsed = Ripper.sexp(File.read(file_name))
+      end
+
+      def determine_definitions_and_references
+        tree = parsed[1] # [:program, [..] ]
+        definitions_and_references = scan(tree)
+        @definitions = definitions_and_references[:definitions]
+        @references = definitions_and_references[:references]
+      end
+
+      private
+
+      def scan(tree)
+        # we want to match a module, a class, constant or method definition
+        # and add those to definitions
+        definitions = {}
+        references = []
+
+        tree.each do |sexp|
+          definition, body = match_definitions(sexp)
+          if definition
+            definitions[definition] = {}
+
+            if body
+              result = scan(body)
+              definitions[definition] = result[:definitions]
+              result[:references].each do |r|
+                # Each reference is a tuple of the name of the reference, and
+                # its named scope
+                references << [r[0], r[1].unshift(definition)]
+              end
+            end
+          end
+        end
+
+        # we want to match constants and ident references and add those to references
+        tree.each do |sexp|
+          match_references(sexp).each do |reference|
+            references << [reference,[]] if reference
+          end
+        end
+
+        # inside those matches we want to recurse to find more definitions and references
+        {
+          definitions: definitions,
+          references: references
+        }
+      end
+
+      # Note these will have to be expressions that can be evaluated at compile time
+      def match_references(sexp)
+        return [] unless sexp.is_a?(Array)
+
+        case sexp.first
+        when :const_path_ref
+          match_const_path(sexp)
+        when :call
+          match_call(sexp)
+        when :top_const_ref
+          match_top_const(sexp)
+        when :var_ref
+          match_var_ref(sexp)
+        when :assign
+          match_assign(sexp)
+        when :method_add_arg
+          match_method_add_arg(sexp)
+        when :method_add_block
+          match_method_add_block(sexp)
+        when :array
+          match_array(sexp)
+        when :opassign
+          match_opassign(sexp)
+        when :def
+          # TODO this isn't really an expression, but we'll have to modify the
+          # definitions thing to explore scopes without names or something to
+          # remove this here
+          match_def_references(sexp)
+        when :command
+          match_command(sexp)
+        when :class
+          match_class_inheritance(sexp)
+        when :void_stmt, :field, :var_field, :@int, :vcall, :if, :string_literal,
+             :binary, :aref, :fcall
+          [] # TODO implement most of these
+        else
+          warn "Don't know how to find const_ref in: #{sexp.first}"
+          []
+        end
+      end
+
+      def match_def_references(sexp)
+        # [:def,
+        #   [:@ident, "initialize", [5, 8]],
+        #   [:params, nil, nil, nil, nil, nil, nil, nil],
+        #   [:bodystmt, [[:void_stmt]], nil, nil, nil]]
+        params = sexp[2][1..-1] # Todo find consts in params
+        body = sexp[3][1]
+        body.map {|e| match_references(e)}.flatten
+      end
+
+      def match_assign(sexp)
+        match_references(sexp[1]) + match_references(sexp[2])
+      end
+
+      def match_var_ref(sexp)
+        # [:var_ref, [:@const, "Game", [5, 12]]]
+        if sexp[1][0] == :@const
+          [] << sexp[1][1]
+        else
+          # Are there other cases?
+          []
+        end
+      end
+
+      def match_top_const(sexp)
+        # [:top_const_ref, [:@const, "Accessiblity", [3, 14]]]
+        [] << "::" + sexp[1][1]
+      end
+
+      def match_call(sexp)
+        match_references(sexp[1]) # TODO continue implementation
+      end
+
+      def match_method_add_arg(sexp)
+        # [:method_add_arg,
+        #   [:call, [ ...]],
+        #   [:arg_paren, [:args_add_block, [[:var_ref,
+        result = match_references(sexp[1])
+        if sexp[2] && sexp[2][0] == :args_add_block
+          result += sexp[2][1].map {|e| match_references(e)}.flatten
+        end
+        result
+      end
+
+      def match_method_add_block(sexp)
+        # [:method_add_block,
+        #   [:call, [ .. ] ]
+          # [:brace_block, [:block_var, [:params, [[:@ident, "beer", [22, 22]]], ..], false], [[:method_add_arg, [:call, [:call, [:var_ref, [:@kw, "self", [22, 28]]], :".", [:@ident, "view", [22, 33]]], :".", [:@ident, "addAnnotation", [22, 38]]], [:arg_paren, [:args_add_block, [[:var_ref, [:@ident, "beer", [22, 52]]]], false]]]]]
+        # ]
+        result = match_references(sexp[1])
+        if sexp[2] && sexp[2][0] == :brace_block
+          result += sexp[2][2].map {|e| match_references(e)}.flatten
+        end
+        result
+      end
+
+      # [:const_path_ref, [:var_ref, [:@const, "B1", [1, 0]]], [:@const, "B2", [1, 4]]]
+      def match_const_path(sexp)
+        # TODO what if it somehow references a constant that's not part of the
+        # const path?
+        prefix = match_references(sexp[1])
+        suffix = sexp[2][1]
+        [] << (prefix << suffix).join("::")
+      end
+
+      def match_command(sexp)
+        # TODO research what else can be in a command
+        args = sexp[2]
+        args[1].flat_map {|a| match_references(a)}.compact
+      end
+
+      def match_class_inheritance(sexp)
+        match_references(sexp[2]) # TODO untested
+      end
+
+      def match_array(sexp)
+        sexp[1].flat_map {|a| match_references(a)}.compact
+      end
+
+      def match_opassign(sexp)
+        # [:opassign, [:var_field, [..], [:@op, "||=", [51, 29]], [:call, .. ]]]
+        match_references(sexp[1]) + match_references(sexp[3])
+      end
+
+      def match_definitions(sexp)
+        return [] unless sexp.is_a?(Array)
+
+        case sexp.first
+        when :class
+          match_class_def(sexp)
+        when :module
+          match_module_def(sexp)
+        when :assign
+          match_constant_def(sexp)
+        when :def
+          match_method_def(sexp)
+        when :void_stmt
+          []
+        else
+          warn "Don't know how to find definition for #{sexp.first}"
+          []
+        end
+      end
+
+
+      def match_method_def(sexp)
+        nil # todo
+      end
+
+      def match_class_def(sexp)
+        # [:class, [:const_ref ... ], nil, ... ]
+        [] << resolve_const_ref(sexp[1]) << sexp[3][1]
+      end
+
+      def match_module_def(sexp)
+        # [:module, [:const_ref ... ], ... ]
+        [] << resolve_const_ref(sexp[1]) << sexp[2][1]
+      end
+
+      def match_constant_def(sexp)
+        if sexp[1].first == :var_field &&
+           sexp[1][1].first == :@const
+           [] << sexp[1][1][1]
+        end
+      end
+
+      def resolve_const_ref(sexp)
+        if sexp[0] == :const_ref
+          # [:const_ref, [:@const, "View", [1, 6]]]
+          sexp[1][1]
+        else
+          raise "name not a const_ref"
+        end
+      rescue
+        puts "Could not resolve name: #{ sexp.inspect }"
+      end
     end
 
     # TODO move to config.rb (or something..)
@@ -102,14 +335,14 @@ module Motion; module Project
 
       # we make a Hash of each definition, to the file it is defined in
       build_context.each do |f|
-        add_definitions(definitions, f[:name], f[:definitions], "")
+        add_definitions(definitions, f.file_name, f.definitions, "")
       end
 
       # for each reference in each file, we want to get the file that has it defined
       build_context.each do |f|
-        references = f[:references]
-        dependants[f[:name]] =
-          references.map {|r| get_dependency(f[:name], definitions, r)}.compact.uniq.select{|r| r != f[:name] }
+        references = f.references
+        dependants[f.file_name] =
+          references.map {|r| get_dependency(f.file_name, definitions, r)}.compact.uniq.select{|r| r != f.file_name }
       end
 
       dependants.keep_if {|_,v| !v.empty? }
@@ -153,233 +386,6 @@ module Motion; module Project
         definitions[fullname] = filename # TODO multiple file definitions
         add_definitions(definitions, filename, nested, fullname)
       end
-    end
-
-    def determine_definitions_and_references(file)
-      definitions_and_references = scan(file[:parsed][1]) # [:program, [..] ]
-
-      file.merge! definitions_and_references
-
-      file
-    end
-
-    def scan(tree)
-      # we want to match a module, a class, constant or method definition
-      # and add those to definitions
-      definitions = {}
-      references = []
-
-      tree.each do |sexp|
-        definition, body = match_definition(sexp)
-        if definition
-          definitions[definition] = {}
-
-          if body
-            result = scan(body)
-            definitions[definition] = result[:definitions]
-            result[:references].each do |r|
-              # Each reference is a tuple of the name of the reference, and
-              # its named scope
-              references << [r[0], r[1].unshift(definition)]
-            end
-          end
-        end
-      end
-
-      # we want to match constants and ident references and add those to references
-      tree.each do |sexp|
-        next unless sexp
-        found_references =
-          match_class_inheritance(sexp) ||
-          match_command(sexp) ||
-          match_expression(sexp)
-
-        next unless found_references
-
-        found_references.each do |reference|
-          references << [reference,[]] if reference
-        end
-      end
-
-      # inside those matches we want to recurse to find more definitions and references
-
-      {
-        definitions: definitions,
-        references: references
-      }
-    end
-
-    # Note these will have to be expressions that can be evaluated at compile time
-    def match_expression(sexp)
-      # can a named scope be created inside an expression? if so
-      # then this means we're going to have to track both at the
-      # same time :(
-      match_const_path(sexp) ||
-        match_call(sexp) ||
-        match_top_const(sexp) ||
-        match_var_ref(sexp) ||
-        match_assign(sexp) ||
-        match_method_add_arg(sexp) ||
-        match_method_add_block(sexp) ||
-        match_array(sexp) ||
-        match_opassign(sexp) ||
-        match_def_references(sexp) || # todo this isn't really an expression, but we'll have to modify the definitions thing to explore scopes without names or something to remove this here
-        [] # etc..
-    end
-
-    def match_def_references(sexp)
-      # [:def,
-      #   [:@ident, "initialize", [5, 8]],
-      #   [:params, nil, nil, nil, nil, nil, nil, nil],
-      #   [:bodystmt, [[:void_stmt]], nil, nil, nil]]
-      if sexp && sexp[0] == :def
-        params = sexp[2][1..-1] # Todo find consts in params
-        body = sexp[3][1]
-        body.map {|e| match_expression(e)}.flatten
-      end
-    end
-
-    def match_assign(sexp)
-      if sexp && sexp[0] == :assign
-        match_expression(sexp[1]) + match_expression(sexp[2])
-      end
-    end
-
-    def match_var_ref(sexp)
-      # [:var_ref, [:@const, "Game", [5, 12]]]
-      if sexp && sexp[0] == :var_ref
-        if sexp[1][0] == :@const
-          [] << sexp[1][1]
-        end
-      end
-    end
-
-    def match_top_const(sexp)
-      # [:top_const_ref, [:@const, "Accessiblity", [3, 14]]]
-      if sexp && sexp[0] == :top_const_ref
-        [] << "::" + sexp[1][1]
-      end
-    end
-
-    def match_call(sexp)
-      if sexp && sexp[0] == :call
-        match_expression(sexp[1]) # TODO continue implementation
-      end
-    end
-
-    def match_method_add_arg(sexp)
-      # ,[:method_add_arg,
-        # [:call, [ ...]],
-        # [:arg_paren, [:args_add_block, [[:var_ref,
-      if sexp && sexp[0] == :method_add_arg
-        result = match_expression(sexp[1])
-        if sexp[2] && sexp[2][0] == :args_add_block
-          result += sexp[2][1].map {|e| match_expression(e)}.flatten
-        end
-      end
-      result
-    end
-
-    def match_method_add_block(sexp)
-      # [:method_add_block,
-      #   [:call, [ .. ] ]
-        # [:brace_block, [:block_var, [:params, [[:@ident, "beer", [22, 22]]], ..], false], [[:method_add_arg, [:call, [:call, [:var_ref, [:@kw, "self", [22, 28]]], :".", [:@ident, "view", [22, 33]]], :".", [:@ident, "addAnnotation", [22, 38]]], [:arg_paren, [:args_add_block, [[:var_ref, [:@ident, "beer", [22, 52]]]], false]]]]]
-      # ]
-      if sexp && sexp[0] == :method_add_block
-        result = match_expression(sexp[1])
-        if sexp[2] && sexp[2][0] == :brace_block
-          result += sexp[2][2].map {|e| match_expression(e)}.flatten
-        end
-      end
-      result
-    end
-
-    # [:const_path_ref, [:var_ref, [:@const, "B1", [1, 0]]], [:@const, "B2", [1, 4]]]
-    def match_const_path(sexp)
-      if sexp && sexp[0] == :const_path_ref
-        # TODO what if it somehow references a constant that's not part of the
-        # const path?
-        prefix = match_expression(sexp[1])
-        suffix = sexp[2][1]
-        [] << (prefix << suffix).join("::")
-      end
-    end
-
-    def match_command(sexp)
-      if sexp && sexp[0] == :command
-        # TODO research what else can be in a command
-        args = sexp[2]
-        args[1].flat_map {|a| match_expression(a)}.compact
-      end
-    end
-
-    def match_class_inheritance(sexp)
-      if sexp && sexp.first == :class
-        match_expression(sexp[2]) # TODO untested
-      end
-    end
-
-    def match_array(sexp)
-      if sexp && sexp[0] == :array
-        sexp[1].flat_map {|a| match_expression(a)}.compact
-      end
-    end
-
-    def match_definition(sexp)
-      match_class_def(sexp) || match_module_def(sexp) || match_constant_def(sexp) || match_method_def(sexp)
-    end
-
-    def match_opassign(sexp)
-      # [:opassign, [:var_field, [..], [:@op, "||=", [51, 29]], [:call, .. ]]]
-      if sexp && sexp.first == :opassign
-        match_expression(sexp[1]) + match_expression(sexp[3])
-      end
-    end
-
-    def match_method_def(sexp)
-      nil # todo
-    end
-
-    def match_class_def(sexp)
-      # [:class, [:const_ref ... ], nil, ... ]
-      if sexp && sexp.first == :class
-        [] << resolve_const_ref(sexp[1]) << sexp[3][1]
-      end
-    end
-
-    def match_module_def(sexp)
-      # [:module, [:const_ref ... ], ... ]
-      if sexp && sexp.first == :module
-        [] << resolve_const_ref(sexp[1]) << sexp[2][1]
-      end
-    end
-
-    def match_constant_def(sexp)
-      if sexp && sexp.first == :assign &&
-         sexp[1].first == :var_field &&
-         sexp[1][1].first == :@const
-         [] << sexp[1][1][1]
-      end
-    end
-
-    def resolve_const_ref(sexp)
-      if sexp[0] == :const_ref
-        # [:const_ref, [:@const, "View", [1, 6]]]
-        sexp[1][1]
-      else
-        raise "name not a const_ref"
-      end
-    rescue
-      puts "Could not resolve name: #{ sexp.inspect }"
-    end
-
-    def parse(file)
-      {
-        name: file,
-        parsed: Ripper.sexp(File.read(file)),
-        definitions: {},
-        references: {}
-      }
     end
   end
 end; end
